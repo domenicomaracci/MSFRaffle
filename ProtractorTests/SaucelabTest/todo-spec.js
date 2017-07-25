f12d4cb00fad96424eb7e616939e46b2f4255393
domenicomaracci82 <domenico.maracci@ca.com>
2017-07-25 14:47:55 +0200

    na

--- ProtractorTests/SaucelabTest/todo-spec.js
+++ ProtractorTests/SaucelabTest/todo-spec.js
@@ -3,6 +3,6 @@ describe('CDBU Modern Software Factory UI Test - Pre-Prod Environment', function
     browser.ignoreSynchronization = true;
     browser.get('http://MSFDemo.gpvn33bcqg.us-east-2.elasticbeanstalk.com');
     element(by.css('[name="name"]')).sendKeys('Selenium Test User');
-    element(by.buttonText('Submit')).click();
+    element(by.buttonText('submit')).click();
     });
 });
